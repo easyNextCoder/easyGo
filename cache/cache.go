5b4056ac8aaedb231f79adcbf953ba16540aa79d
xuyongkang <2561258938@qq.com>
2025-02-17 16:38:54 +0800

    cache

--- cache/cache.go
+++ cache/cache.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"fmt"
 	"github.com/patrickmn/go-cache"
+	"strconv"
 	"time"
 )
 
@@ -16,25 +17,62 @@ type User struct {
 	age  int
 }
 
-func work() {
-	//d = 0表示使用默认过期时间
+func cacheValue() {
+	//d = 0  表示使用默认过期时间
+	//d = -1 表示永远不过期
 	gcache.Set("user1", User{
 		name: "kk",
 		age:  18,
 	}, 0)
 
-	//d = -1表示永远不过期
-
-	loadData()
+	<-time.After(time.Second * 3)
+	val, ok := gcache.Get("user1")
+	if ok {
+		fmt.Printf("loadValue success %+v\n", val.(User))
+	} else {
+		fmt.Printf("loadValue failed\n")
+	}
 }
 
-func loadData() {
+func cachePointer() {
+
+	user := &User{
+		name: "kk",
+		age:  18,
+	}
+
+	gcache.Set("user1", user, 0)
 
 	<-time.After(time.Second * 3)
 	val, ok := gcache.Get("user1")
 	if ok {
-		fmt.Printf("loadData success %+v\n", val.(User))
+		got, _ := val.(*User)
+		got.name = "kkk"
+		fmt.Printf("loadPointer success got %+v origin %+v\n", got, user)
 	} else {
-		fmt.Printf("loadData failed\n")
+		fmt.Printf("loadPointer failed\n")
 	}
+}
+
+func cacheItemCountDelete() {
+
+	count := 0
+	for count < 100 {
+		count++
+		gcache.Set(strconv.Itoa(count), count, 0)
+	}
+
+	num := gcache.ItemCount()
+
+	fmt.Printf("cache itemCount %d\n", num)
+
+	count = 0
+	for count < 100 {
+		count++
+		gcache.Delete(strconv.Itoa(count))
+	}
+
+	num = gcache.ItemCount()
+
+	fmt.Printf("cache itemCount after Delete %d\n", num)
 }
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -7,11 +7,39 @@ func Test_work(t *testing.T) {
 		name string
 	}{
 		// TODO: Add test cases.
-		{name: "测试本地cache的过期"},
+		{name: "测试本地cache value值的过期"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cacheValue()
+		})
+	}
+}
+
+func Test_cachePointer(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{name: "测试本地cache pointer值的过期"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			work()
+			cachePointer()
+		})
+	}
+}
+
+func Test_cacheCountDelete(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+		{name: "测试本地cache ItemCount() Delete() 函数"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cacheItemCountDelete()
 		})
 	}
 }
